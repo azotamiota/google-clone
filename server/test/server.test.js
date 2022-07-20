37213c531e876a4a50651f61f958075eac063db1
azotamiota <azotamiota@gmail.com>
2022-07-20 12:38:56 +0100

    add single result tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -15,15 +15,29 @@ describe("API test", () => {
         api.close(done)
     })
 
-    it('1. Retrieve status 200 on "/"', (done) => {
+    it('1. Retrieve status 200 at "/"', (done) => {
         supertest(api).get('/')
             .expect(200, done)
     })
 
     it('2. Retrieve all results at "/results"', async () => {
         const allResults = await supertest(api).get('/results')
-        console.log('allresults: ', allResults.body)
         expect(allResults.body['4']['title']).toEqual('Kiwi')
+    })
+
+    it('3. Retrieve status 200 at "/results/random"', (done) => {
+        supertest(api).get('/results/random')
+            .expect(200, done)
+    })
+
+    it('4. Retreieve a sinlge result at "/results/:id"', async () => {
+        const singleResult = await supertest(api).get('/results/6')
+            expect(singleResult.text).toMatch(/freecodecamp/)
+    })
+
+    it('5. Upon requesting non-existing result retrieve status 404 message at "/results/:id"', (done) => {
+        supertest(api).get('/results/989898989889')
+            .expect(404, done)
     })
 
 })
