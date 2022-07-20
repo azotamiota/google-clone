7445af8eb458be9ec6dc1357c210b42015323324
azotamiota <azotamiota@gmail.com>
2022-07-20 10:45:12 +0100

    Add mock data

--- server/data.js
+++ server/data.js
@@ -1,3 +1,12 @@
-const results = {title: "YouTube",
-url: "https://www.youtube.com",
-tags: ["video", "popular", "streaming"]}
+const results = {"result-1": {"title": "YouTube", url: "https://www.youtube.com", tags: ["video", "popular", "streaming"]},
+"result-2": {"title": "LinkedIn", url: "https://www.linkedin.com", tags: ["social media", "jobs", "connections"]},
+"result-3": {"title": "Cruise Ship Tracker", url: "https://www.cruisemapper.com", tags: ["cruise ship", "map", "live track"]},
+"result-4": {"title": "Kiwi", url: "https://www.kiwi.com", tags: ["travel", "flights", "compare"]},
+"result-5": {"title": "BBC UK", url: "https://www.bbc.co.uk", tags: ["news", "politics", "UK"]},
+"result-6": {"title": "freeCodeCamp", url: "https://www.freecodecamp.org", tags: ["coding", "free", "programming"]},
+"result-7": {"title": "NHS 111", url: "https://111.nhs.uk/", tags: ["111", "medical advice", "help"]},
+"result-8": {"title": "Royal Flying Doctors Service", url: "https://www.flyingdoctor.org.au/", tags: ["doctors", "flying", "emergency"]},
+"result-9": {"title": "Love Volunteers", url: "https://www.lovevolunteers.org/", tags: ["volunteer", "global", "empathy"]},
+"result-10": {"title": "Delish", url: "https://www.delish.com/", tags: ["cooking", "recipe", "food"]}}
+
+module.exports = data
