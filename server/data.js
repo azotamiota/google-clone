0a5342ebd56a103feaaafbc5265d1980c39fbe85
azotamiota <azotamiota@gmail.com>
2022-07-20 11:49:42 +0100

    update data, add new route

--- server/data.js
+++ server/data.js
@@ -1,12 +1,12 @@
-const results = {"result-1": {"title": "YouTube", url: "https://www.youtube.com", tags: ["video", "popular", "streaming"]},
-"result-2": {"title": "LinkedIn", url: "https://www.linkedin.com", tags: ["social media", "jobs", "connections"]},
-"result-3": {"title": "Cruise Ship Tracker", url: "https://www.cruisemapper.com", tags: ["cruise ship", "map", "live track"]},
-"result-4": {"title": "Kiwi", url: "https://www.kiwi.com", tags: ["travel", "flights", "compare"]},
-"result-5": {"title": "BBC UK", url: "https://www.bbc.co.uk", tags: ["news", "politics", "UK"]},
-"result-6": {"title": "freeCodeCamp", url: "https://www.freecodecamp.org", tags: ["coding", "free", "programming"]},
-"result-7": {"title": "NHS 111", url: "https://111.nhs.uk/", tags: ["111", "medical advice", "help"]},
-"result-8": {"title": "Royal Flying Doctors Service", url: "https://www.flyingdoctor.org.au/", tags: ["doctors", "flying", "emergency"]},
-"result-9": {"title": "Love Volunteers", url: "https://www.lovevolunteers.org/", tags: ["volunteer", "global", "empathy"]},
-"result-10": {"title": "Delish", url: "https://www.delish.com/", tags: ["cooking", "recipe", "food"]}}
+const results = {"1": {"title": "YouTube", url: "https://www.youtube.com", tags: ["video", "popular", "streaming"]},
+"2": {"title": "LinkedIn", url: "https://www.linkedin.com", tags: ["social media", "jobs", "connections"]},
+"3": {"title": "Cruise Ship Tracker", url: "https://www.cruisemapper.com", tags: ["cruise ship", "map", "live track"]},
+"4": {"title": "Kiwi", url: "https://www.kiwi.com", tags: ["travel", "flights", "compare"]},
+"5": {"title": "BBC UK", url: "https://www.bbc.co.uk", tags: ["news", "politics", "UK"]},
+"6": {"title": "freeCodeCamp", url: "https://www.freecodecamp.org", tags: ["coding", "free", "programming"]},
+"7": {"title": "NHS 111", url: "https://111.nhs.uk/", tags: ["111", "medical advice", "help"]},
+"8": {"title": "Royal Flying Doctors Service", url: "https://www.flyingdoctor.org.au/", tags: ["doctors", "flying", "emergency"]},
+"9": {"title": "Love Volunteers", url: "https://www.lovevolunteers.org/", tags: ["volunteer", "global", "empathy"]},
+"10": {"title": "Delish", url: "https://www.delish.com/", tags: ["cooking", "recipe", "food"]}}
 
 module.exports = results
--- server/server.js
+++ server/server.js
@@ -12,5 +12,14 @@ app.get('/results', (req, res) => {
     res.json(data)
 })
 
+app.get('/results/:id', (req, res) => {
+    const id = req.params.id
+    if (!data[id]) {
+        res.status(404).send('<h1 style="text-align : center; padding: 1em">No results found</h1>')
+    } else {
+        res.status(200).json(data[id])
+    }
+})
+
 
 module.exports = app
